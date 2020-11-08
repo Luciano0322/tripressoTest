6281bd6399c822fb5e00710ce0375a0734781728
Luciano <majorass206@gmail.com>
2020-11-08 14:34:46 +0800

    stuckByRating

--- tripresso/src/App.js
+++ tripresso/src/App.js
@@ -2,70 +2,58 @@ import React, { Component, useEffect, useState } from 'react';
 import { listData } from './API';
 
 function App () {
-  const [rawDatas, setDatas] = useState([]);
+  const [rawDatas, setDatas] = useState(null);
 
   const getDatas = async () => {
     const rawDatas = await listData();
     setDatas(rawDatas);
+    console.log(rawDatas);
   }
 
   useEffect(() =>{
     getDatas()
   }, [])
-  
-  // let cardInfo = {
-  //   // "tour_list":{
-  //   //   id,
-  //   //   title,
-  //   //   rating,
-  //   //   agency,
-  //   //   tour_days,
-  //   //   min_price,
-  //   //   tags,
-  //   //   group,
-  //   //   image_url,
-  //   //   tour_detail_url
-  //   // }
-  // }
 
-  // rawData.data.forEach((item, i) => {
-  //   if(cardInfo[item.tour_list] == undefined){
-  //     cardInfo[item.tour_list] = {
-  //       id:'',
-  //       title:'',
-  //       rating:'',
-  //       agency:'',
-  //       tour_days:'',
-  //       min_price:'',
-  //       tags:'',
-  //       group:'',
-  //       image_url:'',
-  //       tour_detail_url:'',
-  //     }
-  //   }
-  //   cardInfo[item.tour_list] = {
-  //     id:cardInfo[item.tour_list].id,
-  //     title:cardInfo[item.tour_list].rating,
-  //     agency:cardInfo[item.tour_list].agency,
-  //     tour_days:cardInfo[item.tour_list].tour_days,
-  //     min_price:cardInfo[item.tour_list].min_price,
-  //     tags:cardInfo[item.tour_list].tags,
-  //     group:cardInfo[item.tour_list].group,
-  //     image_url:cardInfo[item.tour_list].image_url,
-  //     tour_detail_url:cardInfo[item.tour_list].tour_detail_url
-  //   }
-  // });
-
-  // console.log(cardInfo);
+  //rating排序
+  const ratSortInc = rawDatas && rawDatas.data.tour_list.sort((a,b) => {
+    if (a.rating > b.rating) {
+      return 1;
+    } else {
+      return -1;
+    }
+  })
+  const ratSortDec = rawDatas && rawDatas.data.tour_list.sort((a,b) =>{
+    if (a.rating < b.rating) {
+      return 1;
+    } else {
+      return -1;
+    }
+  })
+  console.log(ratSortDec);   
+  console.log(ratSortInc);   
 
   return (
     <div className="App">
-      
-      <ul>
-        {rawDatas.map(tour_list => (
-        <li key={tour_list.toString()}>{tour_list.title}</li>
-        ))}
-      </ul>
+      {
+        rawDatas && rawDatas.data.tour_list.map((tour_list) => (
+        <div key={tour_list.id}>
+          <a href={tour_list.tour_detail_url}><img src={tour_list.image_url} width="150" /></a>
+          <p>{tour_list.rating}</p>
+          <h3>{tour_list.title}</h3>
+          {tour_list.tags.map((tags) => 
+            <span>{tags}</span>
+          )}
+          {tour_list.group.map((group)=>
+          <ul key={group.id}>
+            <li>{group.date}</li>
+            <li>{group.quantity}</li>
+          </ul>
+          )}
+
+        </div>
+
+        ))
+      }
     </div>
   )
 }
